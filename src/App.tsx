5751e35f23eb8ad75cbe179e64cb39d39700b222
Maciej Waligora <maciej.waligora.1991@gmail.com>
2024-06-12 19:02:39 +0200

    feat: added frame counter component and firefox debugger luncher

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css';
 import { GameCanvas } from './Components/GameCanvas';
 import { Game, GameFactory } from './GameFactory';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createRef } from 'react';
 import { config as gameConfig } from './Config/GameConfig';
 import { Scaler } from "gamecraft-utils";
+import { FrameCounter } from './Components/FrameCounter';
 
 
 
 function App() {
   const [game, setGame] = useState<Game | null>(null);
+  const frameCounterRef = createRef<FrameCounter>();
 
   useEffect(() => {
     async function initializeGame() {
@@ -24,7 +26,7 @@ function App() {
   }
   return (
     <div className="App">
-      {game ? <GameCanvas pixiApp={game.renderer} /> : <p>Loading...</p>}
+      {game ? <><FrameCounter ref={frameCounterRef} renderer={game.renderer}/><GameCanvas pixiApp={game.renderer} /></>: <p>Loading...</p>}
     </div>
   );
 }
